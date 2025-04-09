592cfdb9cb122a55dfb5af7052dad971645587dd
centSadili <sadiliva@students.nu-moa.edu.ph>
2025-04-09 09:44:35 +0800

    Push the whole Ordering Sytem Project

---
+++ Sadili_Vincent_INF222_Finals/src/sadili_vincent_inf222_finals/SuperAdmin.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sadili_vincent_inf222_finals;
+
+/**
+ *
+ * @author vincent
+ */
+public class SuperAdmin {
+    public static String username="Vincent";
+    public static String password="1234";
+}
---
+++ Sadili_Vincent_INF222_Finals/src/sadili_vincent_inf222_finals/UserAccount.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sadili_vincent_inf222_finals;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author vincent
+ */
+public class UserAccount {
+    public static ArrayList<String> Fullname = new ArrayList<String>();
+    public static ArrayList<String> username = new ArrayList<String>();
+    public static ArrayList<String> password = new ArrayList<String>();
+    public static ArrayList<String> birthday = new ArrayList<String>();
+    public static ArrayList<String> age = new ArrayList<String>();
+    public static ArrayList<String> accountStat = new ArrayList<String>();
+    public static ArrayList<String> gender= new ArrayList<String>();
+    public static ArrayList<String> job= new ArrayList<String>();
+    
+    public static ArrayList<String> AccountLog= new ArrayList<String>();
+    public static ArrayList<String> Date= new ArrayList<String>();
+    public static ArrayList<String> TimeIn= new ArrayList<String>();
+    public static ArrayList<String> Timeout= new ArrayList<String>();
+    public static int attempts = 1;
+    
+    public static ArrayList<String> TranNO= new ArrayList<String>();
+    public static ArrayList<String> RefNo= new ArrayList<String>();
+    public static ArrayList<String> CusName= new ArrayList<String>();
+    public static ArrayList<String> Order= new ArrayList<String>();
+    public static ArrayList<String> TimeOrdered= new ArrayList<String>();
+}
